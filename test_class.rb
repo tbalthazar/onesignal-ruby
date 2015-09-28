a082cd73f345d61d605e905d63b98ddaea7523e9
Thomas Balthazar <thomas@balthazar.info>
2015-09-28 20:52:51 +0200

    Add notify in the example file

--- test_class.rb
+++ test_class.rb
@@ -3,28 +3,56 @@ require 'dotenv'
 
 Dotenv.load
 api_key = ENV['ONESIGNAL_API_KEY']
-app_id = ENV['ONESIGNAL_APP_ID']
-device_token = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc3"
+@app_id = ENV['ONESIGNAL_APP_ID']
+@device_token = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc3"
 
 OneSignal::OneSignal.api_key = api_key
 
-params = {
-  app_id: app_id,
-  device_type: 0,
-  identifier: device_token,
-  tags: {
-    user_id: 'test2'
+def create_player
+  params = {
+    app_id: @app_id,
+    device_type: 0,
+    identifier: @device_token,
+    tags: {
+      user_id: 'test2'
+    }
   }
-}
 
-begin 
-  response = OneSignal::Player.create(params)
-  puts "code : #{response.code}"
-  puts "message : #{response.message}"
-  puts "body : #{response.body}"
-rescue OneSignal::OneSignalError => e
-  puts "-- message : #{e.message}"
-  puts "-- status : #{e.http_status}"
-  puts "-- body : #{e.http_body}"
+  begin 
+    response = OneSignal::Player.create(params)
+    puts "code : #{response.code}"
+    puts "message : #{response.message}"
+    puts "body : #{response.body}"
+  rescue OneSignal::OneSignalError => e
+    puts "-- message : #{e.message}"
+    puts "-- status : #{e.http_status}"
+    puts "-- body : #{e.http_body}"
+  end
 end
 
+def notify
+  params = {
+    app_id: @app_id,
+    contents: {
+      en: 'hey buddy'
+    },
+    tags: [
+      key: 'user_id',
+      relation: '=',
+      value: "test"
+    ]
+  }
+
+  begin 
+    response = OneSignal::Notification.create(params)
+    puts "code : #{response.code}"
+    puts "message : #{response.message}"
+    puts "body : #{response.body}"
+  rescue OneSignal::OneSignalError => e
+    puts "-- message : #{e.message}"
+    puts "-- status : #{e.http_status}"
+    puts "-- body : #{e.http_body}"
+  end
+end
+
+notify
