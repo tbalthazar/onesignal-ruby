50c4bb9b3901b80994d64e594c987a10dd2e8e21
Jordan Moncharmont <jormon@gmail.com>
2015-12-16 14:58:20 -0800

    add a test

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,7 +5,8 @@ class PlayerTest < MiniTest::Test
   def setup
     base_url = "https://onesignal.com/api/v1/players"
     @create_uri = URI.parse(base_url)
-
+    @csv_export_uri = URI.parse(base_url + "/csv_export")
+    
     @player_id = "fake-id-123"
     @update_uri = URI.parse(base_url + "/#{@player_id}")
 
@@ -17,7 +18,7 @@ class PlayerTest < MiniTest::Test
     OneSignal::OneSignal.api_key = @api_key
   end
 
-  def test_create_raises_error    
+  def test_create_raises_error
     response = mock_response_ko
     OneSignal::OneSignal.expects(:send_post_request)
                         .with(uri: @create_uri, body: @params)
@@ -25,6 +26,15 @@ class PlayerTest < MiniTest::Test
     assert_raises OneSignal::OneSignalError do
       OneSignal::Player.create(params: @params)
     end
+  end
+
+  def test_csv_export
+    @app_id = "foobar"
+    response = mock_response_ok
+    OneSignal::OneSignal.expects(:send_post_request)
+                        .with(uri: @csv_export_uri, body: {app_id: @app_id})
+                        .returns(response)
+    assert_equal response, OneSignal::Player.csv_export(app_id: @app_id)
   end
 
   def test_create
@@ -35,7 +45,7 @@ class PlayerTest < MiniTest::Test
     assert_equal response, OneSignal::Player.create(params: @params)
   end
 
-  def test_update_raises_error    
+  def test_update_raises_error
     response = mock_response_ko
     OneSignal::OneSignal.expects(:send_put_request)
                         .with(uri: @update_uri, body: @params)
