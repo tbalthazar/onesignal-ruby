1a166b4b0771f062526d1ac6ed22965207a3f72a
Javier Aranda <javier.aranda@famaex.com>
2022-05-27 10:06:21 +0200

    Store original message in error exception

--- lib/one_signal/errors/one_signal_error.rb
+++ lib/one_signal/errors/one_signal_error.rb
@@ -1,11 +1,13 @@
 module OneSignal
   class OneSignalError < StandardError
     attr_reader :message
+    attr_reader :original_message
     attr_reader :http_status
     attr_reader :http_body
 
     def initialize(message: nil, http_status: nil, http_body: nil)
       @message = message
+      @original_message = message
       @http_status = http_status
       @http_body = http_body
       @message += " - http status : #{@http_status}" unless @http_status.nil?
